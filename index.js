6aa3e636f112e8e905846b373e4e318fc5be2596
hasan-ullah <mdhasanullah124@gmail.com>
2022-05-25 17:44:04 +0600

    reviw api create

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require("express");
 const app = express();
 const cors = require("cors")
@@ -8,6 +9,42 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.svxxg.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+    try{
+        await client.connect();
+
+        const productCollections = client.db("solar_motor").collection("products");
+        const reviewCollections = client.db("solar_motor").collection("review");
+
+        // all products get api
+        app.get('/product', async(req,res)=>{
+            const query = {}
+            const products = await productCollections.find(query).toArray();
+            res.send(products)
+        })
+        // oen product get api
+        app.get('/product/:id', async(req,res)=>{
+            const id = req.params.id
+            const query = {_id:ObjectId(id)}
+            const products = await productCollections.findOne(query);
+            res.send(products)
+        })
+        // all review get api
+        app.get('/review', async(req,res)=>{
+            const query = {}
+            const review = await reviewCollections.find(query).toArray();
+            res.send(review)
+        })
+
+    }finally{
+        // await client.close();
+    }
+}run().catch(console.dir);
 
 
 
